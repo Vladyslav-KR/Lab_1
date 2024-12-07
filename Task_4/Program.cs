b3d7bd426f1dafea55d87a418bce170dcd393f2b
Vladyslav-KR <vladplay707@gmail.com>
2024-12-07 17:29:39 +0100

    Add files via upload

---
+++ Task_4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Викликаємо метод для налаштування кодування
+            BaseConsole.Initialize();
+
+            // Введення даних про користувача
+            Console.Write("Введіть логін: ");
+            string login = Console.ReadLine();
+
+            Console.Write("Введіть ім'я: ");
+            string firstName = Console.ReadLine();
+
+            Console.Write("Введіть прізвище: ");
+            string lastName = Console.ReadLine();
+
+            Console.Write("Введіть вік: ");
+            int age = int.Parse(Console.ReadLine());
+
+            // Створення екземпляра класу User
+            User user = new User(login, firstName, lastName, age);
+
+            // Виведення інформації про користувача
+            user.DisplayUserInfo();
+        }
+    }
+}
---
+++ Task_4/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Task_4
+{
+    class User
+    {
+        public string Login { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public DateTime DateOfRegistration { get; }
+
+        // Конструктор для ініціалізації даних користувача
+        public User(string login, string firstName, string lastName, int age)
+        {
+            Login = login;
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            DateOfRegistration = DateTime.Now; // ініціалізуємо дату заповнення анкети
+        }
+
+        // Метод для виведення інформації про користувача
+        public void DisplayUserInfo()
+        {
+            Console.WriteLine("Інформація про користувача:");
+            Console.WriteLine($"Логін: {Login}");
+            Console.WriteLine($"Ім'я: {FirstName}");
+            Console.WriteLine($"Прізвище: {LastName}");
+            Console.WriteLine($"Вік: {Age}");
+            Console.WriteLine($"Дата заповнення анкети: {DateOfRegistration}");
+        }
+    }
+}
