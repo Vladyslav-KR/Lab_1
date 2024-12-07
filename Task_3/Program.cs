66cc778a5eb22c484d7aa866c368edf69b52207a
Vladyslav-KR <vladplay707@gmail.com>
2024-12-07 17:48:54 +0100

    Add files via upload

---
+++ Task_3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Task_3
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Викликаємо налаштування для кодування
+            BaseConsole.Initialize();
+
+            // Введення даних
+            Console.Write("Введіть прізвище співробітника: ");
+            string lastName = Console.ReadLine();
+
+            Console.Write("Введіть ім'я співробітника: ");
+            string firstName = Console.ReadLine();
+
+            Console.Write("Введіть посаду (junior, middle, senior): ");
+            string position = Console.ReadLine();
+
+            Console.Write("Введіть стаж роботи (роки): ");
+            int experience = int.Parse(Console.ReadLine());
+
+            // Створення екземпляра класу Employee
+            Employee employee = new Employee(lastName, firstName)
+            {
+                Position = position,
+                Experience = experience
+            };
+
+            // Розрахунок окладу та податку
+            employee.CalculateSalary();
+
+            // Виведення інформації
+            employee.DisplayInfo();
+        }
+    }
+}
